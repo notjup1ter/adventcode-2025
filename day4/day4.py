057a3ea30d7355c732de924d3218a1dc01ecdbf1
jayden <mug4ct@virginia.edu>
2025-12-10 08:52:09 -0500

    day 4, part two completed

--- day4/day4.py
+++ day4/day4.py
@@ -1,24 +1,42 @@
 #idea, make a movement array indicating directions in which forklifts can move
 moves = [(1,0), (-1, 0), (0, 1), (0,-1), (-1, 1), (-1, -1), (1, 1), (1 , -1)]
 res = 0
-with open("paperrolls.txt", "r") as file:
-    arr = file.read().splitlines()
+with open("paperrolls.txt", "r+") as file:
+    arr = list(file.read().splitlines())
+    print(arr)
 
+con = True
+while con:
+    temp_res = 0 #reset only when con (non-zero pass) is met
     for i in range(len(arr)):
         for j in range(len(arr[0])):
             char = arr[i][j]
-            print(char)
             adj_count = 0
             if char == '.':
                 continue
-        
+            
             for dx, dy in moves:
                 nx, ny = j + dx, i + dy
                 if nx >= 0 and nx <= len(arr[0]) - 1 and ny >= 0 and ny <= len(arr) - 1:
-                    if arr[ny][nx] == "@":
+                    if arr[ny][nx] == "@" or arr[ny][nx] == "x":
                         adj_count += 1
             if adj_count < 4:
-                res += 1
+                row = list(arr[i])
+                row[j] = "x"
+                arr[i] = "".join(row)
+                temp_res += 1
+    print(temp_res)
+    if temp_res == 0: #when no new paper rolls can be collected
+        con = False
+    res += temp_res
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            if arr[i][j] == "x":
+                row = list(arr[i])
+                row[j] = "."
+                arr[i] = "".join(row)
+
+                
 
 print(res)
 
